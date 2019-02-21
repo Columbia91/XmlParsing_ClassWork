7a2a9f9c2fbd872455b372c67d5ac193bfadb45a
Columbia91 <alpha.z91@mail.ru>
2019-02-21 20:21:43 +0600

    Добавьте файлы проекта.

---
+++ XmlParsing/City.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlParsing
+{
+    public class City
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public bool IsCapital { get; set; }
+        public string CountryName { get; set; }
+
+        public City()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ XmlParsing/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Xml;
+
+namespace XmlParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load("data.xml");
+
+            var rootElement = xmlDocument.DocumentElement;
+
+            City city = new City
+            {
+                Name = rootElement.GetElementsByTagName("name")[0].InnerText,
+                Population = int.Parse(rootElement.GetElementsByTagName("population")[0].InnerText),
+                IsCapital = bool.Parse(rootElement.GetElementsByTagName("isCapital")[0].InnerText),
+                CountryName = rootElement.GetElementsByTagName("countryName")[0].InnerText
+            };*/
+            City city = new City
+            {
+                Name = "Алматы",
+                Population = 2000000,
+                IsCapital = false,
+                CountryName = "Казахстан"
+            };
+            XmlDocument xmlDocument = new XmlDocument();
+            var rootElement = xmlDocument.CreateElement("city");
+
+            var nameElement = xmlDocument.CreateElement("name");
+            nameElement.InnerText = city.Name;
+            rootElement.AppendChild(nameElement);
+
+            var populationElement = xmlDocument.CreateElement("population");
+            populationElement.InnerText = city.Population.ToString();
+            rootElement.AppendChild(populationElement);
+            // и так далее и т.п.
+
+            xmlDocument.AppendChild(rootElement);
+            xmlDocument.Save("data2.xml");
+
+            Console.ReadLine();
+        }
+    }
+}
